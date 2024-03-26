f3461097538a5dc1461bb2335bd5d6b4a770dffb
Evan Cheng <evanchengdesign@gmail.com>
2024-03-26 14:04:36 -0400

    add fragment to return and rename title in index.html

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,10 +3,12 @@ import HornedBeast from './HornedBeast';
 
 const Gallery = () => {
   return (
+    <>
     <div className="gallery-container">
       <HornedBeast title="Eastern Black Rhino" imageUrl="src/images/eastern-black-rhino.png" description="An Eastern Black Rhino" />
       <HornedBeast title="Texas Horned Frog" imageUrl="src/images/texas-horned-lizard.png" description="A Texas Horned Frog." />
     </div>
+    </>
   );
 };
 
