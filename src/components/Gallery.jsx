6c15eb15f5bb6d43112f97e35c3ae4e48958dee9
Evan Cheng <evanchengdesign@gmail.com>
2024-03-25 22:34:10 -0400

    Fixed the linking of images and components

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,7 +4,7 @@ import HornedBeast from './HornedBeast';
 function Gallery() {
   return (
     <div>
-      <HornedBeast title="Eastern Black Rhino" imageUrl="src/images/eastern-black-rhino.png" description="A Eastern Black Rhino" />
+      <HornedBeast title="Eastern Black Rhino" imageUrl="src/images/eastern-black-rhino.png" description="An Eastern Black Rhino" />
       <HornedBeast title="Texas Horned Frog" imageUrl="src/images/texas-horned-lizard.png" description="A Texas Horned Frog." />
     </div>
   );
