9b663c696bdbd96f0b3937ecd47536133ed40b49
Evan Cheng <evanchengdesign@gmail.com>
2024-03-25 22:37:46 -0400

    refactor to arrow functions

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
 
-function Gallery() {
+const Gallery = () => {
   return (
     <div>
       <HornedBeast title="Eastern Black Rhino" imageUrl="src/images/eastern-black-rhino.png" description="An Eastern Black Rhino" />
       <HornedBeast title="Texas Horned Frog" imageUrl="src/images/texas-horned-lizard.png" description="A Texas Horned Frog." />
     </div>
   );
-}
+};
 
 export default Gallery;
