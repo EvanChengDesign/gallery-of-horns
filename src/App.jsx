7afcf62bbd4e9e43aec8ae8952ed6145c60ec072
Evan Cheng <evanchengdesign@gmail.com>
2024-03-27 17:38:20 -0400

    updated and moved json into app to handle the state and send top down, vs gallery handling. added modal function.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Gallery from './components/Gallery';
+import SelectedBeast from './components/SelectedBeast';
 import './index.css'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import beasts from './components/data.json';
 
 function App() {
+
+  const [selectedBeast, setSelectedBeast] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleBeastClick = (beast) => {
+    console.log(beast);
+    setSelectedBeast(beast);
+    setShowModal(true);
+  };
+
+  const handleClose = () => setShowModal(false);
+
+
   return (
     <div className="main-container">
       <Header />
-      <Gallery />
+      <Gallery beasts={beasts} onBeastClick={handleBeastClick} />
+      <SelectedBeast beast={selectedBeast} show={showModal} handleClose={handleClose} />
       <Footer />
     </div>
   );
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
-import beasts from './data.json';
 
-const Gallery = () => {
+
+const Gallery = ({ beasts, onBeastClick }) => {
   return (
     <div className="gallery-container">
       {beasts.map((beast, index) => (
+        <div key={index} onClick={() => onBeastClick(beast)}>
         <HornedBeast
           key={index}
           title={beast.title}
           imageUrl={beast.image_url}
           description={beast.description}
         />
-      ))}
     </div>
+      ))}
+   </div>   
   );
 };
 
