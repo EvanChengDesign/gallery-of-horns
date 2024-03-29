d8e93738ddd7e2cd719e64ad30d68593e689107e
Evan Cheng <evanchengdesign@gmail.com>
2024-03-28 22:17:38 -0400

    add filter

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,42 @@ import SelectedBeast from './components/SelectedBeast';
 import './index.css'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import beasts from './components/data.json';
+import FilterHornedBeasts from './components/FilterHornBeasts';
 
 function App() {
 
   const [selectedBeast, setSelectedBeast] = useState(null);
   const [showModal, setShowModal] = useState(false);
+  const [hornFilter, setHornFilter] = useState('');
 
   const handleBeastClick = (beast) => {
     console.log(beast);
-    setSelectedBeast(beast);
+    setSelectedBeast(beast)
     setShowModal(true);
   };
 
   const handleClose = () => setShowModal(false);
 
+  const handleFilterChange = (selectedHorns) => {
+    setHornFilter(selectedHorns);
+  };
 
-  return (
+  let filteredBeasts;
+  if (hornFilter === '') {
+    filteredBeasts = beasts;
+
+  } else {
+
+    filteredBeasts = beasts.filter(function(beast) {
+      return beast.horns.toString() === hornFilter;
+    });
+  }
+
+    return (
     <div className="main-container">
       <Header />
-      <Gallery beasts={beasts} onBeastClick={handleBeastClick} />
+      <FilterHornedBeasts onFilterChange={handleFilterChange} />
+      <Gallery beasts={filteredBeasts} onBeastClick={handleBeastClick} />
       <SelectedBeast beast={selectedBeast} show={showModal} handleClose={handleClose} />
       <Footer />
     </div>
